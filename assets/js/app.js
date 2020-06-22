72a3f1320c2cf8b83a2da2e65618ce82885729e0
karma-websites <67168746+karma-websites@users.noreply.github.com>
2020-06-22 20:40:45 +0300

    Upload all files

---
+++ assets/js/app.js
@@ -0,0 +1,95 @@
+$(function() {
+	
+	
+	
+	/* Fixed Header */
+	let header = $("#header");
+	let intro = $("#intro");
+	let introH = intro.innerHeight();
+	let scrollPosition = $(window).scrollTop();
+	
+	checkScroll(scrollPosition, introH);
+	
+	$(window).on("scroll resize", function(e) {
+		introH = intro.innerHeight();
+		scrollPosition = $(this).scrollTop();
+		checkScroll(scrollPosition, introH);	
+	});
+	
+	function checkScroll(scrollPosition, introH) {
+		if( scrollPosition >= introH ) {
+			header.addClass("fixed");
+		} else {
+			header.removeClass("fixed");
+		}
+	}
+	
+	
+	
+	/* Smooth scroll */
+	$("[data-scroll]").on("click", function(e) {
+		event.preventDefault();
+		
+		let $this = $(this),
+			blockId = $this.data('scroll'),
+			blockOffset = $(blockId).offset().top;
+		
+		$("nav a").removeClass("active");
+		$this.addClass("active");
+		
+		nav.removeClass("active"); // Забираем класс active у навигации
+        navToggle.removeClass("active");
+		
+		$("html, body").animate({
+			scrollTop: blockOffset + 10
+		}, 900);
+	});
+	
+	
+	
+	/* Menu nav toggle */
+	let nav = $("#nav");
+	let navToggle = $("#navToggle");
+	
+	navToggle.on("click", function(e) {
+		event.preventDefault();
+		
+		$(this).toggleClass("active");
+		nav.toggleClass("active");
+	});
+	
+	
+	
+	/* Collapse */
+	$("[data-collapse]").on("click", function(e) {
+		event.preventDefault();
+		
+		let $this = $(this),
+			blockId = $this.data('collapse');
+		
+		$(blockId).slideToggle();
+	});
+	
+	
+	
+	/* reviews: https://kenwheeler.github.io/slick/ */
+	$("[data-slider]").slick({
+  		infinite: true,
+  		slidesToShow: 1,
+  		slidesToScroll: 1,
+		fade: false
+	});	
+	
+	
+	
+}); 
+
+
+
+
+
+
+
+
+
+
